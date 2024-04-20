acc9b92750220dcc9f2970b69b5d0e7c3662bd2f
Manu Bhat <43832426+enigmurl@users.noreply.github.com>
2024-04-20 13:08:14 -0700

    Merge pull request #1 from cse110-sp24-group26/manu
    Add calendar and boilerplate

---
+++ calendar/calendar.js
@@ -0,0 +1,55 @@
+import { numDays, dayOfWeek } from './util.js';
+
+const DAYS_IN_WEEK = 7;
+const COLUMN_HEADERS = ["S", "M", "T", "W", "T", "F", "S"];
+
+class Calendar extends HTMLElement {
+	constructor() {
+		super()
+	}
+
+	retree() {
+		const year = this.getAttribute("year");
+		const month = this.getAttribute("month");
+		
+		const daysInMonth = numDays(year, month);
+		const padding = dayOfWeek(year, month, 1);
+
+		const items = [];
+
+		/* column headers */
+		for (let i = 0; i < DAYS_IN_WEEK; ++i) {
+			const header = document.createElement("h2");
+			header.appendChild(document.createTextNode(COLUMN_HEADERS[i]));
+			items.push(header);
+		}
+
+		/* padding */
+		for (let i = 0; i < padding; ++i) {
+			const pad = document.createElement("span");
+			pad.className = "pad-day"
+			items.push(pad);
+		}
+
+		for (let i = 1; i <= daysInMonth; ++i) {
+			const day = document.createElement("my-day");
+			day.setAttribute("day", i);
+			day.setAttribute("href", "https://www.google.com");
+			items.push(day);
+		}
+
+		/* replace children */
+		this.querySelectorAll("h2, span.pad-day, my-day").forEach(e => this.removeChild(e));
+		items.forEach(day => this.appendChild(day));
+	}
+
+	connectedCallback() {
+		this.retree();
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.retree();
+	}
+}
+
+customElements.define("my-calendar", Calendar);
