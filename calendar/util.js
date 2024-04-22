2f868a0a661d242b5fcd058187abf8620f33b69d
e-celik <145171936+e-celik@users.noreply.github.com>
2024-04-22 16:44:37 -0700

    Update util.js with numDays
    Wrote code for numDays, haven't tested yet.

--- calendar/util.js
+++ calendar/util.js
@@ -1,6 +1,41 @@
+/*
+Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, 
+but these centurial years are leap years if they are exactly divisible by 400. For example, the years 1700, 1800, 
+and 1900 are not leap years, but the years 1600 and 2000 are.
+*/
 /* Month is given as January, February, etc, year is given as int */
 export function numDays(year, month) {
-	return 28;
+	switch(month) {
+		case("January"):
+		case("March"):
+		case("May"):
+		case("July):
+		case("August"):
+		case("October"):
+		case("December"):
+			return 31;
+			break;
+		case("April"):
+		case("June"):
+		case("September"):
+		case("November"):
+			return 30;
+			break;
+		case("February"):
+			let four = (year % 4 == 0);
+			let hundred = (year % 100 == 0);
+			let fourhundred = (year % 400 == 0);
+			if (four && !(hundred && !fourhundred)) //Boolean reduction from rule at top
+			{
+				return 29;
+			}
+			else
+			{
+				return 28;
+			}
+			break;
+	}
+		
 }
 
 /* Day is given as one indexed day, return value is zero-indexed day of week */
