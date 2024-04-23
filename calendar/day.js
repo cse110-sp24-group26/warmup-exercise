4da3d762d7fba7045461b03064d32d08860c20da
Suhaib Chowdhury <ogs1492@gmail.com>
2024-04-23 15:52:02 -0700

    Some code cleanup

--- calendar/day.js
+++ calendar/day.js
@@ -7,7 +7,10 @@ class Day extends HTMLElement {
 		const dayNum = parseInt(this.getAttribute("day"));
 		// Should add month attribute
 		const monthNum = parseInt(this.getAttribute("month"));
-		const href = this.getAttribute("href");
+		// Should add year attribute
+		const yearNum = parseInt(this.getAttribute("year"));
+		// Not needed
+		//const href = this.getAttribute("href");
 
 		this.appendChild(document.createElement("button"));
 		const button = document.getElementsByTagName("button")[dayNum - 1];
@@ -19,14 +22,8 @@ class Day extends HTMLElement {
 				selected.removeAttribute("id");
 			}
 			this.setAttribute("id", "selected")
-			button.setAttribute("style", "border:10px, border-color: 141418")
 			var win = window.open("index.html","","popup, width=800, height = 600")
 			win.document.write(`<h1> Day ${dayNum} </h1>`)
-			const form = document.createElement("img");
-			//document.appendChild(form)
-			form.innerHTML = `<h1> Day ${dayNum} </h1>`
-			calendar = document.getElementById("main-calendar");
-			calendar.after(form)
 		}
 	}
 }
