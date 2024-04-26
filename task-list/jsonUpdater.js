a42dcf37413525b78ddcc484c7f626c7cc9067b3
jasonho318 <jasonho318@gmail.com>
2024-04-26 13:29:54 -0700

    reformat documentation, move testing to its own folder, jsonUpdater error handling

--- task-list/jsonUpdater.js
+++ task-list/jsonUpdater.js
@@ -1,4 +1,4 @@
-fetch("./multiple_tasks.json")
+fetch("./testing/multiple_tasks.json")
   .then(function (response) {
     return response.json();
   })
@@ -8,28 +8,92 @@ fetch("./multiple_tasks.json")
 
     // adds each subtask to page
     for (let mainTask of taskList.tasks) {
-      // displaying each main task title
-      out += `
+      if (!mainTask.hasOwnProperty("title")) {
+        //checking if mainTask entry has a title key
+        throw new Error(
+          `
+          Invalid .JSON file, please ensure it is in the following format:
+            
+          {  
+            "tasks": [
+              {
+                "title": "",
+                "subtasks" : [
+                  {
+                    "subtitle" : "",
+                    "due_date" : "",
+                    "description" : ""
+                  },
+                  {
+                    "subtitle" : "",
+                    "due_date" : "",
+                    "description" : ""
+                  }
+                ]
+              }
+            }
+        `
+        );
+      } else {
+        // displaying each main task title
+        out += `
                 <div class="task-list">
                     <h2 class="task-list-title">${mainTask["title"]}</h2>
                     <ul class="tasks">
             `;
-      //iterating through the subtask of each main task
-      for (let subtask of mainTask.subtasks) {
-        out += `
+        //iterating through the subtask of each main task
+        for (let subtask of mainTask.subtasks) {
+          if (
+            !(
+              subtask.hasOwnProperty("subtitle") &&
+              subtask.hasOwnProperty("due_date") &&
+              subtask.hasOwnProperty("description")
+            )
+          ) {
+            throw new Error(
+              `
+              Invalid .JSON file, please ensure it is in the following format:
+                
+              {  
+                "tasks": [
+                  {
+                    "title": "",
+                    "subtasks" : [
+                      {
+                        "subtitle" : "",
+                        "due_date" : "",
+                        "description" : ""
+                      },
+                      {
+                        "subtitle" : "",
+                        "due_date" : "",
+                        "description" : ""
+                      }
+                    ]
+                  }
+                }
+            `
+            );
+          } else {
+            out += `
                     <li class="task">
                         <h3 class="task-title">${subtask["subtitle"]}</h3>
                         <p class="task-description">${subtask["description"]}</p>
                         <p class="due-date">${subtask["due_date"]}</p>
                     </li>
                 `;
-      }
+          }
+        }
 
-      out += `
+        out += `
                     </ul>
                 </div>
             `;
-    }
 
+      }
+    }
     placeholder.innerHTML = out;
+  })
+  .catch(function (error) {
+    alert("An error occured: " + error.message);
   });
