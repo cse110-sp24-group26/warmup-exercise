eafc5bc2c2c6140bc575dbd0c6478d72ffcf390c
Angelo Avanzado <38778212+AAvanzado@users.noreply.github.com>
2024-04-25 21:11:08 -0700

    added json format check + inncorrect json file

--- task-list/testing_script.js
+++ task-list/testing_script.js
@@ -1,26 +1,42 @@
-fetch('./multiple_tasks.json')
+fetch('./missing_subtask_info.json')
     .then(function (response){
         return response.json();
     })
     .then(function (taskList){
         let placeholder = document.querySelector("#data-output");
         let out = "";
-        
+
         // adds each subtask to page
         for(let mainTask of taskList.tasks){
             // displaying each main task title
-            out += `
-                <hr>
-                <h2>${mainTask['title']}</h2>
-            `
-            //iterating through the subtask of each main task
-            for(let subtask of mainTask.subtasks){ 
+            if(!(mainTask.hasOwnProperty("title"))){
+                window.alert("Invalid json file: please check the formating of your json file");
+                break;
+            }
+            else{
                 out += `
-                    <h3>${subtask['subtitle']}</h3>
-                    <p>${subtask['due_date']}</p>
-                    <p>${subtask['description']}</p>
-                `;
-            }    
+                    <hr>
+                    <h2>${mainTask['title']}</h2>
+                `
+                //iterating through the subtask of each main task
+                for(let subtask of mainTask.subtasks){ 
+                    if(!(subtask.hasOwnProperty("subtitle") 
+                            && subtask.hasOwnProperty("due_date") 
+                            && subtask.hasOwnProperty("desription"))){
+                        window.alert("Invalid json file: please check the formating of your json file");        
+                            }
+                        
+                    else{
+                        out += `
+                        <h3>${subtask['subtitle']}</h3>
+                        <p>${subtask['due_date']}</p>
+                        <p>${subtask['description']}</p>
+                    `;
+                    }
+                    
+                }  
+            }
+              
         }
         
 
